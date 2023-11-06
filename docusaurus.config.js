a740c79759e44996d2c030eadf6148ea5218eae5
Tianchun Luo <luotianchun123@qq.com>
2023-11-06 23:05:59 +0800

    feat: update home page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,58 +1,58 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'RisingWave Crash Course (Preview)',
-  tagline: 'The hands-on tutorial for data streaming developers!',
-  favicon: 'img/favicon.ico',
+  title: "RisingWave Crash Course (Preview)",
+  tagline: "The hands-on tutorial for data streaming developers!",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://tutorials.risingwave.com/',
+  url: "https://tutorials.risingwave.com/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/risingwavelabs/risingwave-tutorials/tree/main/',
+            "https://github.com/risingwavelabs/risingwave-tutorials/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/risingwavelabs/risingwave-tutorials/tree/main/',
+            "https://github.com/risingwavelabs/risingwave-tutorials/tree/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -60,9 +60,9 @@ const config = {
 
   plugins: [
     [
-      '@docusaurus/plugin-google-gtag',
+      "@docusaurus/plugin-google-gtag",
       {
-        trackingID: 'G-L2NLRWNE5T',
+        trackingID: "G-L2NLRWNE5T",
       },
     ],
   ],
@@ -71,30 +71,30 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/thumbnail.png",
       navbar: {
-        title: 'RisingWave Crash Course',
+        title: "RisingWave Crash Course",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Get Started',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Get Started",
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/risingwavelabs/risingwave',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/risingwavelabs/risingwave",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           // {
           //   title: 'Docs',
@@ -106,19 +106,19 @@ const config = {
           //   ],
           // },
           {
-            title: 'Links',
+            title: "Links",
             items: [
               {
-                label: 'Official Website',
-                href: 'https://risingwave.com/',
+                label: "Official Website",
+                href: "https://risingwave.com/",
               },
               {
-                label: 'Slack Community',
-                href: 'https://www.risingwave.com/slack',
+                label: "Slack Community",
+                href: "https://www.risingwave.com/slack",
               },
               {
-                label: 'Twitter',
-                href: 'https://www.risingwave.com/twitter',
+                label: "Twitter",
+                href: "https://www.risingwave.com/twitter",
               },
             ],
           },
